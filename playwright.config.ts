be7777e9298c8e9d51be8a34fab540991741edcc
bagavathiraja <98267897+bagavathiraja@users.noreply.github.com>
2023-09-04 15:56:16 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ const config: PlaywrightTestConfig = {
     //     // },
     // ],
 
-//   testMatch: ["azure.spec.ts"],
+  testMatch: ["tests/azure.spec.ts"],
     use: {
         viewport: null,
         headless: !true,
